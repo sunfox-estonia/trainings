9c622df16f1a6be273993b533ba0d63bc885da09
Vitgor Sunfox <vitgor@sunfox.ee>
2018-11-08 18:28:29 +0200

    v 0.2 - MySQL configuration in a separate file + README update.

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+  * Database configuration file. MySQL database can be used only.
+  */
+
+/*
+ * Database server location: host and port.
+ */
+$dbhost = 'localhost';
+$dbpost = '3306';
+/*
+ * Database name and user cridentials. Please do not use MySQL root-user cridentials to provide an access to database!
+ */
+$dbname = 'mysql_name';
+$dbuser = 'mysql_user';
+$dbpassword = 'mysql_pass';
+
+
+
+
